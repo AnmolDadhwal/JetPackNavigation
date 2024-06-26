2efae69aee2cf9bb03d9ae5027a293e17e1a22da
Anmol Dadhwal <dadhwalanmol08@gmail.com>
2024-06-26 17:34:29 +0530

    commit

--- app/src/main/java/com/task/jetpacknavigation/MainActivity.kt
+++ app/src/main/java/com/task/jetpacknavigation/MainActivity.kt
@@ -7,28 +7,38 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.task.jetpacknavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding:ActivityMainBinding?=null
-    var navController:NavController?=null
+    lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.navController) as NavHostFragment
+        navController=navHostFragment.navController
+        val appBarConfiguration=AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         navController=findNavController(R.id.navController)
-        navController?.addOnDestinationChangedListener{_,destination,_->
+        navController.addOnDestinationChangedListener{_,destination,_->
             when(destination.id){
                 R.id.jetPackNavigation->supportActionBar?.title="Enter Email"
                 R.id.secondFragment->supportActionBar?.title="Enter OTP"
                 R.id.thirdFragment->supportActionBar?.title="Set New Password"
             }
         }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()||super.onSupportNavigateUp()
     }
 }
